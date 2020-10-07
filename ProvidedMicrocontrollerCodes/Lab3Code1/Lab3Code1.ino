dd1ef629fbaec4f8318143107641a6f3130d38f0
Patrick Mayerhofer <patrickmayerhofer@MacBook-Pro-2.local>
2020-10-07 12:04:34 -0700

    Patrick

--- ProvidedMicrocontrollerCodes/Lab3Code1/Lab3Code1.ino
+++ ProvidedMicrocontrollerCodes/Lab3Code1/Lab3Code1.ino
@@ -1,5 +1,8 @@
 /*
  * This code measures an analog signal with a given sampling frequency
+ * 3.3V pin needs to be connected to AREF pin. We do this 
+ * to increase the resolution by changing the analog input 
+ * range from 0-5V to 0-3.3V.
  * Created by Patrick Mayerhofer June 2020
  */
 
@@ -11,6 +14,7 @@ unsigned int ADC_Value = 0;    //ADC current value
 unsigned long last_time = 0;
 void setup() {
   Serial.begin(500000);
+  analogReference(EXTERNAL); // use AREF for reference voltage
 }
 
 
